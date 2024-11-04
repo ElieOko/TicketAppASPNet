52b23fab9599727d7619397d06a21c03c142d066
ElieOko <elieoko100@gmail.com>
2024-11-04 09:30:43 +0100

    feat:controller implementation

---
+++ TicketApp/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketApp.Interfaces;
+using TicketApp.Models;
+
+namespace TicketApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IUser _user;
+        public UserController(IUser user) 
+        {
+            _user = user;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<User>))]
+        public IActionResult GetAll()
+        {
+            var users = _user.GetAll();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            
+            return Ok(users);
+        }
+    }
+}
