aab0064074cde7d85c7eed69a718ce6d5354d136
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-11-05 09:28:15 +0100

    Feat:Add services for hash password and jwt

---
+++ TicketApp/Services/PaaswordHasher.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TicketApp.Services
+{
+    public class PaaswordHasher
+    {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            if (storedSalt == null || storedSalt.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(storedSalt), "Le sel ne peut pas être nul ou vide.");
+            }
+            using (var hmac = new HMACSHA512(storedSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(storedHash);
+            }
+        }
+    }
+}
+
+
+
