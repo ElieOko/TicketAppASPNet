89daa7e910e420a3b5e6d4033f83bd1a6ec0b093
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-12-30 10:13:02 +0100

    Feat: Add services pour calculer la duree comprise entre l'ouverture d'un ticket et la fin d'un paiement

---
+++ TicketApp/Services/CalculDuree.cs
@@ -0,0 +1,15 @@
+﻿namespace TicketApp.Services
+{
+    public static class CalculDuree
+    {
+        public static TimeSpan? CalculerDuree(string heureDebutString, string? heureFinString)
+        {
+            if (TimeSpan.TryParse(heureDebutString, out TimeSpan heureDebut) &&
+                TimeSpan.TryParse(heureFinString, out TimeSpan heureFin))
+            {
+                return heureFin - heureDebut;
+            }
+            return null; 
+        }
+    }
+}
